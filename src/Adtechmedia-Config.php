32ae36074d18a95b9f30dae6cc2267145963a454
yama_gs <yama_gs@groupbwt.com>
2016-10-26 20:01:28 +0300

    fix Adtechmedia-Config.php

--- src/Adtechmedia-Config.php
+++ src/Adtechmedia-Config.php
@@ -6,18 +6,32 @@
  * Date: 17.10.2016
  * Time: 14:01
  */
-class Adtechmedia_Config {
 
-	private static $conf = [
-		'api_end_point' => 'https://api.adtechmedia.io/dev/',
-		'plugin_table_name' => 'adtechmedia',
-		'plugin_cache_table_name' => 'adtechmedia_cache',
-		'maxTries' => 7,
-		'minDelay' => 150000,
-		'factor' => 1.7,
-	];
+/**
+ * @file
+ * Plugin config file
+ */
+class Adtechmedia_Config
+{
 
-	public static function get( $name ) {
-		return self::$conf[ $name ];
-	}
+    /**
+     * @var array plugin config
+     */
+    private static $conf = [
+        'api_end_point' => 'https://api.adtechmedia.io/dev/',
+        'plugin_table_name' => 'adtechmedia',
+        'plugin_cache_table_name' => 'adtechmedia_cache',
+        'maxTries' => 7,
+        'minDelay' => 150000,
+        'factor' => 1.7,
+    ];
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public static function get($name)
+    {
+        return self::$conf[$name];
+    }
 }
